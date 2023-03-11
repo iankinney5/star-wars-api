96c27e268d724c1bb3e587764078a9b42bec41bb
Ian Kinney <94138683+iankinney5@users.noreply.github.com>
2023-03-11 16:09:58 -0500

    Create api_worksheet.js

---
+++ api_worksheet.js
@@ -0,0 +1,5 @@
+// Perform an api call to fetch info on Tattoine (https://swapi.dev/api/planets/1/) and save in variable Tattoine.
+
+// Perform an api call to fetch Obi-Wan Kenobi people data (https://swapi.dev/api/people/1) and save in variable kenobi.
+
+// Create a function to grab 10 characters from the Star Wars API and put in array people.
